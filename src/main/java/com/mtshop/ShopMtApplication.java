786271415bed2cb5e1b3cdc1cefc5b6f57d91af0
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2022-12-30 22:53:44 +0700

    config storage and cloudinary

--- src/main/java/com/mtshop/ShopMtApplication.java
+++ src/main/java/com/mtshop/ShopMtApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+
 public class ShopMtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopMtApplication.class, args);
 	}
+	
+	
 
 }
---
+++ src/main/java/com/mtshop/config/StorageConfig.java
@@ -0,0 +1,27 @@
+package com.mtshop.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import com.mtshop.service.StorageService;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class StorageConfig {
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args -> {
+			storageService.init();
+		});
+	}
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding("UTF-8");
+		return resolver;
+	}
+}
